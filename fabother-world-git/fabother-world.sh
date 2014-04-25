ad6900679b3c1b5a797e4310abfc75c212b170f9
Carsten Teibes <dev@f4ke.de>
2014-04-25 19:04:33 +0200

    cosmetics

--- fabother-world-git/fabother-world.sh
+++ fabother-world-git/fabother-world.sh
@@ -2,4 +2,6 @@
 
 mkdir -p ~/.fabother-world/data
 
-exec /usr/share/fabother-world/fabother-world --datapath=$HOME/.fabother-world/data --savepath=$HOME/.fabother-world "$@"
+exec /usr/share/fabother-world/fabother-world \
+     --datapath=$HOME/.fabother-world/data \
+     --savepath=$HOME/.fabother-world "$@"
